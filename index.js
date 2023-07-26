1a77af0a043a26ab3e9dc267d4efd3d715c05dd5
gowda-raghu <87423535+gowda-raghu@users.noreply.github.com>
2023-07-26 15:59:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+for (var i=0; i<document.querySelectorAll(".drum").length; i++)
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  var b=this.innerHTML;
+  makesound(b);
+  ani(b);
+  });
+document.addEventListener("keydown",function(event){
+  makesound(event.key);
+  ani(event.key);
+});
+function makesound(button){
+  switch (button) {
+    case "w":
+      var audio= new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio= new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio= new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio= new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio= new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio= new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio= new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+}
+}
+
+function ani(key){
+  document.querySelector("."+key).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+key).classList.remove("pressed");
+  },100)
+}
